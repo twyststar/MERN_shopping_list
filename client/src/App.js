b9a2c7800b552331356c4adcb4045276551eade3
Starlynne <lewis.start0905@gmail.com>
2018-08-19 16:07:53 -0700

    Add ShoppingList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppNavBar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavBar />
+        <ShoppingList />
       </div>
     );
   }
